b2575a4fa85f17275c7bbd285095c6f2ff133f28
Nathan Cloutier <nathanc4571@gmail.com>
2025-07-09 19:23:58 -0400

    Basic setup

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+const corsOptions = {
+    origin: true,
+    credentials: true,
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('MongoDB connected');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    })
+}).catch(error => {
+    console.error(error);
+});
